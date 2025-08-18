7ba73f47b96c884c8744f56a80544f3715d5c34e
brudalevante <continentalwl@gmail.com>
2025-08-18 04:27:16 +0200

    Update bpi-r4-openwrt-builder.sh

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -9,7 +9,7 @@ echo "==== 2. CLONA REPOSITORIOS (kernel 6.6.100) ===="
 git clone --branch openwrt-24.10 https://github.com/brudalevante/openwrt-18-08-25.git openwrt || true
 cd openwrt; git checkout 4941509f573676c4678115a0a3a743ef78b63c17; cd -;	# uhttpd: update to Git HEAD (2025-07-06) kernel 6.6.100
 
-git clone https://github.com/brudalevante/mtk-openwrt-feeds-15-08-2025.git mtk-openwrt-feeds || true
+git clone https://github.com/brudalevante/mtk-openwrt-feeds-18-08-2025.git mtk-openwrt-feeds || true
 cd mtk-openwrt-feeds; git checkout 39d725c3e3b486405e6148c8466111ef13516808; cd -; # Refactor wed amsdu init value
 
 echo "39d725" > mtk-openwrt-feeds/autobuild/unified/feed_revision
